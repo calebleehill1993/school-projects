d57beead22cf5b27f7c42eb01d191c1e7181ca85
Caleb Lee Hill <calebleehill1993@gmail.com>
2019-03-28 14:44:56 -0600

    Ch. 2

---
+++ CS240/Android Studio Projects/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by clhill93 on 3/28/19.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
